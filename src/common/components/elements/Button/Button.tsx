56eb4ea6ec00097612cc6e777ffc68589d51bd17
lxsthashir <gurinowich2002@gmail.com>
2023-10-13 00:54:01 +0300

    add button tsx

--- src/common/components/elements/Button/Button.tsx
+++ src/common/components/elements/Button/Button.tsx
@@ -1,5 +1,20 @@
-const Button = () => {
-  return <button></button>;
+import React from 'react';
+import './button.css';
+
+interface ButtonProps {
+  text?: string;
+  onClick?: () => void;
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+      <div className="button-animation">
+      <button className="button-click" onClick={props.onClick}>
+        {props.text}
+      </button>
+      </div>
+  );
 };
 
 export default Button;
+
--- src/common/components/elements/Input/Input.tsx
+++ src/common/components/elements/Input/Input.tsx
@@ -1,21 +1,6 @@
-import './input.css';
-
-interface InputProps {
-  placeholder?: string;
-  value?: string;
-  type?: 'text';
-  label?: string;
-}
-
-const Input = (props:InputProps) => {
-
-  return (
-      <div className={"input-animation"}>
-      {props.label && <label>{props.label}</label>} 
-         <input className="input__animation" {...props} />
-      </div> );
-
+const Input = () => {
+    return <input />;
 };
 
-
 export default Input;
+
