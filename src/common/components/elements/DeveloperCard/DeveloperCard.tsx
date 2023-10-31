232ae9c10d0467e16f34749cfebb524183eb34e7
Roman <gricrom21@gmail.com>
2023-10-31 18:21:27 +0300

    added developer card, change app name, fix svg

---
+++ src/common/components/elements/DeveloperCard/DeveloperCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './developerCard.css';
+
+interface IDeveloperCardProps {
+  jobPosition: string;
+  name: string;
+  photoURL?: string;
+}
+
+export const DeveloperCard = ({ jobPosition, name, photoURL }: IDeveloperCardProps) => {
+  return (
+    <div className='container'>
+      <div className='container_photo'>
+        <img src={photoURL || './assets/Group1.svg'} alt='developer' />
+      </div>
+      <div className='container_description'>
+        <h3>{name}</h3>
+        <span>{jobPosition}</span>
+      </div>
+    </div>
+  );
+};
