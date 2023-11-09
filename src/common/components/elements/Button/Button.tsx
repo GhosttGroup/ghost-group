be60fb0c651664313f0ab7b64caadab7ca343690
lxsthashir <95119110+lxsthashir@users.noreply.github.com>
2023-11-09 20:23:47 +0100

    Custom button (#2)
    * create basic reusable input
    
    * add button tsx
    
    * add css
    
    * hot fix css
    
    * fix: className, const, imports, interface
    
    * fix: css, tsx
    
    * hot fix css
    
    * fixes
    
    * fix
    
    * Delete package-lock.json
    
    ---------
    
    Co-authored-by: Murz4 <cool.murz@gmail.com>
    Co-authored-by: Roman <gricrom21@gmail.com>
    Co-authored-by: Roman Grytskevich <77338277+Roman214-go@users.noreply.github.com>

--- src/common/components/elements/Button/Button.tsx
+++ src/common/components/elements/Button/Button.tsx
@@ -1,5 +1,19 @@
-const Button = () => {
-  return <button></button>;
+import React, { ReactNode, ButtonHTMLAttributes, ReactElement } from 'react';
+
+import './button.css';
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  size?: 'xs' | 'xm';
+}
+
+const Button = (props: IButtonProps): ReactElement => {
+  const { children, size = 'xs' } = props;
+  return (
+    <button className='custom_button' {...props} style={{ padding: size === 'xs' ? '8px 32px' : '16px 48px' }}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
