df0f44be14cf451cc58001d38c3469cdf896023e
lxsthashir <gurinowich2002@gmail.com>
2023-11-09 19:35:55 +0200

    fix

--- src/common/components/elements/Button/Button.tsx
+++ src/common/components/elements/Button/Button.tsx
@@ -1,14 +1,13 @@
 import React, { ReactNode, ButtonHTMLAttributes, ReactElement } from 'react';
 import './button.css';
+
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     onClick?: () => void;}
 
 const Button = (props: IButtonProps): ReactElement => (
     <button className="custom_button" {...props}>
-        <div className="button-content">
             {props.children}
-        </div>
     </button>
 );
 
