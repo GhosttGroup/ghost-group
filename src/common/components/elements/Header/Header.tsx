9a7840b4f7a21d1f0aa4b397a1ef2cedf83aea63
Murz4 <cool.murz@gmail.com>
2023-11-14 00:18:57 +0100

    made TODO

--- src/common/components/elements/Header/Header.tsx
+++ src/common/components/elements/Header/Header.tsx
@@ -4,31 +4,32 @@ import { Link } from 'react-router-dom';
 import { headerArray } from '../../../config';
 import Button from '../Button';
 
-import './header.css';
+import styles from './header.module.css';
+
 
 //TODO set anchors
 const Header = () => {
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='logo-container'>
-          <Link className='link-logo' to='#'>
-            <img className='img-logo' src='/assets/Group3.svg' alt='logo' />
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo_container}>
+          <Link className={styles.link_logo} to='#'>
+            <img className={styles.img_logo} src='/assets/Group3.svg' alt='logo' />
           </Link>
         </div>
-        <nav className='nav'>
-          <ul className='list'>
+        <nav className={styles.nav}>
+          <ul className={styles.list}>
             {headerArray.map((item, index) => (
-              <li className='item' key={index}>
-                <Link className='text-link' to={item.link}>
+              <li className={styles.item} key={index}>
+                <Link className={styles.text_link} to={item.link}>
                   {item.text}
                 </Link>
               </li>
             ))}
           </ul>
         </nav>
-        <div className='button-container'>
-          <Link className='img-link' to='#'>
+        <div className={styles.button_container}>
+          <Link className={styles.img_link} to='#'>
             <img src='/assets/iconButton.svg' alt='iconButton' />
           </Link>
           <Button>Contact us</Button>
