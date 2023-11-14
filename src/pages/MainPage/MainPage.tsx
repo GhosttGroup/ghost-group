2e645e42f1de9f6a5ab28418218b16520d2c9094
Murz4 <cool.murz@gmail.com>
2023-11-14 09:41:36 +0100

    hotfix

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,8 +4,6 @@ import Header from '../../common/components/elements/Header';
 import OurAdvantages from '../../common/components/elements/OurAdvantages';
 import OurTeamBlock from '../../common/components/elements/OurTeamBlock/OurTeamBlock';
 
-
-
 import styles from './MainPage.module.css';
 
 const MainPage = () => {
