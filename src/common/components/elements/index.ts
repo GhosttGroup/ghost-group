d1675ba5429fc35a79632d2fd34cf1c8f05a5a89
Roman <gricrom21@gmail.com>
2023-11-24 18:53:25 +0300

    hotfix

--- src/common/components/elements/index.ts
+++ src/common/components/elements/index.ts
@@ -1,5 +1,6 @@
 import Button from './Button';
 import DeveloperCard from './OurTeamBlock/DeveloperCard';
 import Input from './Input';
+import Accordion from './Accordion';
 
-export { Button, DeveloperCard, Input };
+export { Accordion, Button, DeveloperCard, Input };
